55761e0d172a20dde9d18ca1474a679313677e11
Dane Edwards <daneedwards1@hotmail.com>
2022-07-22 21:28:54 +1000

    added approve script

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # api-sdk
-Examples to interact with the Squid API
+
+## Examples to interact with the Squid API
+
+API endpoint documentation
+https://app.gitbook.com/o/bX90yMGYDBu3T1Hqg4EA/s/tXbXyuWIO2PwzNc5Dets/integrate-squid/api
+
+- install package dependancies "yarn install"
+- configuration in .env file
+- explore the /api/quotes & api/tokens endpoints for supported chains and tokens
+- run approval.ts to approve tokens before any transactions eg "yarn ts-node approve.ts"
+- run transaction.ts to query /api/transaction endpoint and send tokens cross chain ✨
+
+## Notes
+
+Transactions are not sent via the api, the api is a helper endpoint only, transactions are submitted via the callers rpc endpoint.
---
+++ approve.ts
@@ -0,0 +1,55 @@
+import { BigNumber, ethers, utils } from "ethers";
+import { parseUnits } from "ethers/lib/utils";
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const WETHContractAddress = process.env.WETHContractAddress!
+const aUSDContractAddress = process.env.aUSDContractAddress!
+
+const ethRpcEndPoint = process.env.ethRpcEndPoint!
+const avaxRpcEndPoint = process.env.avaxRpcEndPoint!
+
+const wethApprovalAmmont: BigNumber = parseUnits("100", 18)
+const ausdcApprovalAmmont: BigNumber = parseUnits("100", 6)
+
+const privateKey = process.env.privateKey!
+const squidContractAddress = process.env.squidContractAddress!
+
+
+async function approveToken(_tokenAddress: string, _rpc: string, _approvalAmount: BigNumber) {
+    const provider = new ethers.providers.JsonRpcProvider(
+        _rpc
+    );
+
+    const signer = new ethers.Wallet(
+        privateKey,
+        provider
+    );
+    // token contract abi
+    const abi = [
+        "function approve(address _spender, uint256 _value) public returns (bool success)",
+    ];
+
+    const contract = new ethers.Contract(
+        WETHContractAddress,
+        abi,
+        signer
+    );
+    await contract.approve(
+        squidContractAddress,
+        _approvalAmount
+    );
+}
+
+async function main() {
+    console.log('approving weth on eth')
+    await approveToken(WETHContractAddress, ethRpcEndPoint, wethApprovalAmmont);
+    console.log('approving ausdc on eth')
+    await approveToken(aUSDContractAddress, ethRpcEndPoint, ausdcApprovalAmmont);
+}
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
