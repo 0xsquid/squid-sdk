64853a93127175f561911ea40ecce819ae8589a4
juanmavillaraza <juan.villarraza@gmail.com>
2022-08-08 16:06:50 -0300

    feat: add eslint & prettier

--- src/utils/getTokenDataBySymbol.ts
+++ src/utils/getTokenDataBySymbol.ts
@@ -1,9 +1,11 @@
-import { supportedTokens } from "../contants/tokens"
-import { Environments, ITokenData } from "../types"
+import { supportedTokens } from '../contants/tokens'
+import { Environments, ITokenData } from '../types'
 
 /**
- * @param {string} symbol could be an enum as it's more rubost validation 
+ * @param {string} symbol could be an enum as it's more rubost validation
  * @param {Environments} env
  */
-export const getTokenDataBySymbol = (symbol: string, env: Environments): ITokenData | undefined =>
-  supportedTokens[env].find(e => e.symbol === symbol)
+export const getTokenDataBySymbol = (
+  symbol: string,
+  env: Environments
+): ITokenData | undefined => supportedTokens[env].find(e => e.symbol === symbol)
