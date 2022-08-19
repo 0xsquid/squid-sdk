634b4a03a23b0ea47d9bc83b5c6b1bb50169ce0f
0x1NotMe <nme15500@gmail.com>
2022-08-19 15:49:09 +1000

    refactor: token and chains to be {}
    using undefined as fallback breaks the
    entire concepts of type safety.

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import {
   AxelarQueryAPIConfig,
   EvmChain,
   GasToken
-} from '@axelar-network/axelarjs-sdk'
-import { ethers } from 'ethers'
-import axios, { AxiosInstance } from 'axios'
-import * as dotenv from 'dotenv'
+} from "@axelar-network/axelarjs-sdk";
+import { ethers } from "ethers";
+import axios, { AxiosInstance } from "axios";
+import * as dotenv from "dotenv";
 
 import {
   ChainsData,
@@ -15,23 +15,23 @@ import {
   GetRoute,
   GetRouteResponse,
   TokenData
-} from './types'
+} from "./types";
 
-import erc20Abi from './abi/erc20.json'
-import { getChainData } from './utils/getChainData'
-import { getTokenData } from './utils/getTokenData'
+import erc20Abi from "./abi/erc20.json";
+import { getChainData } from "./utils/getChainData";
+import { getTokenData } from "./utils/getTokenData";
 
-dotenv.config()
+dotenv.config();
 
-const baseUrl = process.env.baseUrl
+const baseUrl = process.env.baseUrl;
 
 class SquidSdk {
-  private axiosInstance: AxiosInstance
+  private axiosInstance: AxiosInstance;
 
-  public inited = false
-  public config: Config
-  public tokens: TokenData[] | undefined = undefined
-  public chains: ChainsData | undefined = undefined
+  public inited = false;
+  public config: Config;
+  public tokens: TokenData[] = {} as TokenData[];
+  public chains: ChainsData = {} as ChainsData;
 
   constructor(config: Config) {
     this.axiosInstance = axios.create({
@@ -39,18 +39,15 @@ class SquidSdk {
       headers: {
         // 'api-key': config.apiKey
       }
-    })
-    this.config = config
+    });
+    this.config = config;
   }
 
   public async init() {
-    const response = await this.axiosInstance.get('/api/sdk-info', {
-      params: { env: this.config.environment }
-    })
-
-    this.tokens = response.data.data.tokens
-    this.chains = response.data.data.chains
-    this.inited = true
+    const response = await this.axiosInstance.get("/api/sdk-info", {});
+    this.tokens = response.data.data.tokens;
+    this.chains = response.data.data.chains;
+    this.inited = true;
   }
 
   public setConfig(config: Config) {
@@ -59,106 +56,106 @@ class SquidSdk {
       headers: {
         // 'api-key': config.apiKey
       }
-    })
-    this.config = config
+    });
+    this.config = config;
   }
 
   public async getRoute(params: GetRoute): Promise<GetRouteResponse> {
     if (!this.inited) {
       throw new Error(
-        'SquidSdk must be inited! Please call the SquidSdk.init method'
-      )
+        "SquidSdk must be inited! Please call the SquidSdk.init method"
+      );
     }
 
-    const response = await this.axiosInstance.get('/api/route', { params })
+    const response = await this.axiosInstance.get("/api/route", { params });
 
     return {
       route: response.data.route
-    }
+    };
   }
 
   public async executeRoute({
     signer,
     route
   }: ExecuteRoute): Promise<ethers.providers.TransactionResponse> {
-    const { transactionRequest, params } = route
+    const { transactionRequest, params } = route;
 
     if (!this.inited) {
       throw new Error(
-        'SquidSdk must be inited! Please call the SquidSdk.init method'
-      )
+        "SquidSdk must be inited! Please call the SquidSdk.init method"
+      );
     }
 
     const sourceChain = getChainData(
       this.chains as ChainsData,
       params.sourceChainId
-    )
+    );
     const sourceToken = getTokenData(
       this.tokens as TokenData[],
       params.sourceTokenAddress
-    )
+    );
     const destinationChain = getChainData(
       this.chains as ChainsData,
       params.destinationChainId
-    )
+    );
 
     if (!sourceChain) {
-      throw new Error(`sourceChain not found for ${params.sourceChainId}`)
+      throw new Error(`sourceChain not found for ${params.sourceChainId}`);
     }
 
     if (!sourceToken) {
-      throw new Error(`sourceToken not found for ${params.sourceTokenAddress}`)
+      throw new Error(`sourceToken not found for ${params.sourceTokenAddress}`);
     }
 
     if (!destinationChain) {
       throw new Error(
         `destinationChain not found for ${params.destinationChainId}`
-      )
+      );
     }
 
-    const srcProvider = new ethers.providers.JsonRpcProvider(sourceChain.rpc)
+    const srcProvider = new ethers.providers.JsonRpcProvider(sourceChain.rpc);
     const srcTokenContract = new ethers.Contract(
       sourceToken.address,
       erc20Abi,
       srcProvider
-    )
+    );
 
     const allowance = await srcTokenContract.allowance(
       params.recipientAddress,
       sourceChain.squidContracts.squidMain
-    )
+    );
 
     if (allowance < params.sourceAmount) {
       const contract = new ethers.Contract(
         sourceToken.address,
         erc20Abi,
         signer
-      )
+      );
       await contract.approve(
         sourceChain.squidContracts.squidMain,
         params.sourceAmount
-      )
+      );
     }
 
     const sdk = new AxelarQueryAPI({
       environment: this.config.environment as string
-    } as AxelarQueryAPIConfig)
+    } as AxelarQueryAPIConfig);
 
     const gasFee = await sdk.estimateGasFee(
       sourceChain?.nativeCurrency.name as EvmChain, // EvmChain.ETHEREUM,
       destinationChain?.nativeCurrency.name as EvmChain, // EvmChain.AVALANCHE,
       GasToken.ETH,
       transactionRequest.destinationChainGas
-    )
+    );
 
     const tx = {
       to: sourceChain.squidContracts.squidMain,
       data: transactionRequest.data,
       value: BigInt(gasFee) // this will need to be calculated, maybe by the api, also standarice usage of this kind of values
-    }
+    };
 
-    await signer.signTransaction(tx)
-    return await signer.sendTransaction(tx)
+    await signer.signTransaction(tx);
+    return await signer.sendTransaction(tx);
   }
 
   public async allowance(params: any) {
@@ -208,4 +205,4 @@ class SquidSdk {
   }
 }
 
-export default SquidSdk
+export default SquidSdk;
--- src/test/sdk.spec.ts
+++ src/test/sdk.spec.ts
@@ -1,61 +1,63 @@
-import axios from 'axios'
+import axios from "axios";
 
-import { Environment } from '../types'
-import SquidSdk from '../index'
+import { Environment } from "../types";
+import SquidSdk from "../index";
 
-jest.mock('axios')
+jest.mock("axios");
 
 // const axiosInstance = (axios.create as jest.Mock).mockReturnValue({
 //   get: jest.fn()
 // })
 
-describe('SquidSdk', () => {
-  it('should instance SquidSdk as expected', async () => {
+describe("SquidSdk", () => {
+  it("should instance SquidSdk as expected", async () => {
     const squidSdk = new SquidSdk({
       environment: Environment.LOCAL
-    })
+    });
 
-    expect(squidSdk).toMatchSnapshot()
-    expect(squidSdk.inited).toBe(false)
+    expect(squidSdk).toMatchSnapshot();
+    expect(squidSdk.inited).toBe(false);
     expect(squidSdk.config).toStrictEqual({
       environment: Environment.LOCAL
-    })
-    expect(squidSdk.tokens).toBe(undefined)
-    expect(squidSdk.chains).toBe(undefined)
-  })
+    });
+    expect(squidSdk.tokens).toEqual({});
+    expect(squidSdk.chains).toEqual({});
+  });
 
-  describe('setConfig', () => {
-    it('should set config as expected', () => {
+  describe("setConfig", () => {
+    it("should set config as expected", () => {
       const squidSdk = new SquidSdk({
         environment: Environment.LOCAL
-      })
+      });
 
       squidSdk.setConfig({
         environment: Environment.TESTNET
-      })
+      });
 
       expect(squidSdk.config).not.toStrictEqual({
         environment: Environment.LOCAL
-      })
+      });
       expect(squidSdk.config).toStrictEqual({
         environment: Environment.TESTNET
-      })
-    })
-  })
+      });
+    });
+  });
 
-  describe('axios module', () => {
-    it('get', async () => {
+  describe("axios module", () => {
+    it("get", async () => {
       const axiosInstance = (axios.create as jest.Mock).mockReturnValue({
-        get: jest.fn().mockResolvedValue({ data: 'some data on test' })
-      })
+        get: jest.fn().mockResolvedValue({
+          data: { status: true, data: { chains: [], tokens: [] } }
+        })
+      });
 
       const squidSdk = new SquidSdk({
         environment: Environment.LOCAL
-      })
+      });
 
-      await squidSdk.init()
+      await squidSdk.init();
 
-      console.log('> squidsdk: ', squidSdk.chains)
-    })
-  })
-})
+      console.log("> squidsdk: ", squidSdk.chains);
+    });
+  });
+});
