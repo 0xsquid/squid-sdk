c17a428ef12d0e83a283c9be3d03e31ee22cc5aa
jmd3v <juan.villarraza@gmail.com>
2022-09-06 16:51:37 -0300

    add send only test cases

---
+++ src/scripts/sendOnly.ts
@@ -0,0 +1,47 @@
+import { getSignerForChain, getSendOnly } from "./utils";
+import { ChainName } from "../types";
+import { Squid } from "../index";
+import { ethers } from "ethers";
+import chalk from "chalk";
+
+const executeSendOnly = async (
+  squid: Squid,
+  signer: ethers.Wallet,
+  fromNetwork: ChainName,
+  toNetwork: ChainName,
+  amount: string
+) => {
+  const params = getSendOnly(squid, fromNetwork, toNetwork, amount);
+  console.log("\n");
+  console.log(
+    `> sendOnly: from ${fromNetwork}=>${toNetwork} from ${chalk.green(
+      "Token"
+    )} ${params.sourceTokenAddress} to ${chalk.green("Token")} ${
+      params.destinationTokenAddress
+    }`
+  );
+  const { route } = await squid.getRoute(params);
+  const tx = await squid.executeRoute({
+    signer,
+    route
+  });
+  const txReceipt = await tx.wait();
+  console.log(
+    `> txReceipt: , ${
+      txReceipt.transactionHash
+    }, gasUsed: ${txReceipt.gasUsed.toNumber()} `
+  );
+};
+
+export const sendOnly = async (
+  squid: Squid,
+  src: ChainName,
+  dests: ChainName[],
+  amount: string
+) => {
+  const signer = getSignerForChain(src);
+  for (const dest of dests) {
+    await executeSendOnly(squid, signer, src, dest, amount);
+    await executeSendOnly(squid, signer, src, dest, amount);
+  }
+};
