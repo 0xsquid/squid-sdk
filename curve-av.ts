79f5d0dd5d8ce25e1420ab9a4ee5dbd4b2fddb71
jmd3v <juan.villarraza@gmail.com>
2022-09-27 07:35:33 -0500

    wip

--- curve-av.ts
+++ curve-av.ts
@@ -1,91 +1,113 @@
-import { ethers } from "ethers";
-import { Squid } from "./src";
+import { ethers, utils } from "ethers";
 
+import uniswapV2Router02Abi from "./uniswapV2Like.json";
 import erc20Abi from "./src/abi/erc20.json";
-import abi from "./curveRegistry.json";
+import curveAbi from "./curveRegistry.json";
+import routerAbi from "./SquidRouter.json";
 
-const getSDK = (): Squid => {
-  const squid = new Squid({
-    baseUrl: "http://localhost:3000"
-  });
-  return squid;
-};
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+const USDCE = "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664";
+const WAVAX = "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7";
+
+const AXLUSDC = "0xfaB550568C688d5D8A52C7d794cb93Edc26eC0eC";
+
+const amountIn = "100000000000000000000";
+const deadline = new Date().getTime() + 1e7;
+
+const pangolin = "0xE54Ca86531e17Ef3616d22Ca28b0D458b6C89106";
+const curve = "0xFE90eb3FbCddacD248fAFEFb9eAa24F5eF095778";
+
+const tokenInterface = new ethers.utils.Interface(erc20Abi as any);
+const uniInterface = new utils.Interface(uniswapV2Router02Abi);
+const curveInterface = new ethers.utils.Interface(curveAbi as any);
+
+const squidRouter = "0x116fB500fCb3D8E666Ba861a357be599A64cB92d";
+const squidMulticall = "0x14Aa0C6fEa6F4B0586AEF117D7202a40f302327d";
+
+const provider = new ethers.providers.JsonRpcProvider(
+  process.env.avalanceRpcEndPoint
+);
+const signer = new ethers.Wallet(process.env.privateKey as string, provider);
+
+const squidRouterContract = new ethers.Contract(
+  squidRouter,
+  routerAbi as any,
+  signer
+);
 
 (async () => {
   try {
-    const squid = getSDK();
-    await squid.init();
-    const provider = new ethers.providers.JsonRpcProvider(
-      process.env.avalanceRpcEndPoint
-    );
-    const signer = new ethers.Wallet(
-      process.env.privateKey as string,
-      provider
-    );
-
-    const contract = new ethers.Contract(
-      "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664",
-      erc20Abi,
-      signer
-    );
+    const contract = new ethers.Contract(WAVAX, erc20Abi, signer);
     await contract.approve(
-      "0xFE90eb3FbCddacD248fAFEFb9eAa24F5eF095778",
+      squidRouter,
       "115792089237316195423570985008687907853269984665640564039457584007913129639935"
     );
-    console.log(
-      "balance: ",
-      await contract.balanceOf("0xbb8a6436e0e9a22bb7f1dc76afb4421d8195620e")
-    );
 
-    /* const contract2 = new ethers.Contract(
-      "0xFE90eb3FbCddacD248fAFEFb9eAa24F5eF095778",
-      abi as any,
-      signer
-    );
-    const result = await (
-      await contract2[
-        "exchange(address,address,address,uint256,uint256,address)"
-      ](
-        "0xbb8a6436e0e9a22bb7f1dc76afb4421d8195620e",
-        "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664",
-        "0xfaB550568C688d5D8A52C7d794cb93Edc26eC0eC",
-        100000000,
-        0,
-        signer.address
-      )
-    ).wait(); */
-
-    0:
-    '0xbb8a6436e0e9a22bb7f1dc76afb4421d8195620e'
-    1:
-    '0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664'
-    2:
-    '0xfaB550568C688d5D8A52C7d794cb93Edc26eC0eC'
-    const contractInterface = new ethers.utils.Interface(abi as any);
-    const args = [
-      "0xbb8a6436e0e9a22bb7f1dc76afb4421d8195620e",
-      "0xA7D7079b0FEaD91F3e65f86E8915Cb59c1a4C664",
-      "0xfaB550568C688d5D8A52C7d794cb93Edc26eC0eC",
-      100000000,
-      0,
-      signer.address
+    const calls = [
+      {
+        callType: 0,
+        target: WAVAX,
+        value: 0,
+        callData: tokenInterface.encodeFunctionData("approve", [
+          pangolin,
+          amountIn
+        ]),
+        payload: "0x"
+      },
+      {
+        callType: 0,
+        target: pangolin,
+        value: 0,
+        callData: uniInterface.encodeFunctionData("swapExactTokensForTokens", [
+          amountIn,
+          0,
+          [WAVAX, USDCE],
+          squidRouter,
+          deadline
+        ]),
+        payload: "0x"
+      }
+      /* {
+        callType: 1,
+        target: AXLUSDC,
+        value: 0,
+        callData: tokenInterface.encodeFunctionData("approve", [curve, 0]),
+        payload: utils.defaultAbiCoder.encode(
+          ["address", "uint256"],
+          [AXLUSDC, 1]
+        )
+      },
+      {
+        callType: 1,
+        target: curve,
+        value: 0,
+        callData: curveInterface.encodeFunctionData(
+          "exchange(address,address,address,uint256,uint256,address)",
+          [
+            "0xbb8a6436e0e9a22bb7f1dc76afb4421d8195620e",
+            USDCE,
+            AXLUSDC,
+            0,
+            0,
+            signer.address
+          ]
+        ),
+        payload: utils.defaultAbiCoder.encode(
+          ["address", "uint256"],
+          [AXLUSDC, 2]
+        )
+      } */
     ];
 
-    const data = contractInterface.encodeFunctionData(
-      "exchange(address,address,address,uint256,uint256,address)",
-      args
-    );
-    console.log(data);
-    const tx = {
-      to: "0xFE90eb3FbCddacD248fAFEFb9eAa24F5eF095778",
-      data: data,
-      gasLimit: 60e4 // 600000 gasLimit
-    } as ethers.utils.Deferrable<ethers.providers.TransactionRequest>;
+    const tx = await squidRouterContract
+      .connect(signer)
+      .callBridge(WAVAX, amountIn, "1", signer.address, "axlUSDC", calls);
 
-    const txs = await signer.sendTransaction(tx);
-    const result = await txs.wait();
-
-    console.log(result);
+    console.log("> tx: ", tx);
+    console.log("> result: ", await tx.wait());
   } catch (error) {
     console.error(error);
   }
