42ed8dc9476854f472b5c3fd7acacadb67c4a95d
0x1NotMe <2715331+0x1NotMe@users.noreply.github.com>
2023-04-20 18:58:06 +1000

    fix: add bridgeOnly property (#107)

--- src/0xsquid/v1/tokens.spec.ts
+++ src/0xsquid/v1/tokens.spec.ts
@@ -17,6 +17,7 @@ describe("tokens", () => {
       {
         chainId: 1,
         address: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+        bridgeOnly: true,
         name: "Wrapped ETH",
         symbol: "WETH",
         decimals: 18,
