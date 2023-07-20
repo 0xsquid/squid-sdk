4cee2befd96613b7464c7d25110bbd99571e0cdc
0x1NotMe <nme15500@gmail.com>
2023-07-20 22:59:35 +1000

    fix: type issue

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,20 +1,45 @@
 import { ChainData, Token } from "@0xsquid/squid-types";
 import { AxiosRequestHeaders } from "axios";
+import { ErrorType, SquidError } from "error";
+import { Config } from "types";
 
 export const getTokenData = (
   tokens: Token[],
   address: string,
-  chainId: string
-): Token | undefined =>
-  tokens.find(
+  chainId: string,
+  config: Config
+): Token => {
+  const token = tokens.find(
     e =>
       e.address.toLowerCase() === address?.toLowerCase() && e.chainId == chainId
   );
+  if (!token) {
+    throw new SquidError({
+      message: `Could not find token with address ${address} on chain ${chainId}`,
+      errorType: ErrorType.ValidationError,
+      logging: config.logging,
+      logLevel: config.logLevel
+    });
+  }
+  return token;
+};
 
 export const getChainData = (
   chains: ChainData[],
-  chainId: string
-): ChainData | undefined => chains.find(chain => chain.chainId == chainId);
+  chainId: string,
+  config: Config
+): ChainData => {
+  const chain = chains.find(chain => chain.chainId == chainId);
+  if (!chain) {
+    throw new SquidError({
+      message: `Could not find chain with ${chainId}`,
+      errorType: ErrorType.ValidationError,
+      logging: config.logging,
+      logLevel: config.logLevel
+    });
+  }
+  return chain;
+};
 
 export const getHeaderTracker = (headers: AxiosRequestHeaders) => {
   return {
