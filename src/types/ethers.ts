f78a0b0074770631bc57f8d60b62983f053f7b8b
jmd3v <juan.villarraza@gmail.com>
2023-09-05 15:17:05 -0300

    improve wallet type

--- src/types/ethers.ts
+++ src/types/ethers.ts
@@ -5,7 +5,10 @@ export type Contract = ethers.Contract;
 export type ContractRunner = ethers.ContractRunner;
 export type Interface = ethers.Interface;
 
-export type EvmWallet = ethers.Wallet | ethers.Signer;
+export type WalletV6 = ethers.Wallet;
+export type WalletV5 = ethers.Signer;
+
+export type EvmWallet = WalletV6 | WalletV5;
 
 export type Transaction = ethers.Transaction;
 export type TransactionResponse = ethers.TransactionResponse;
