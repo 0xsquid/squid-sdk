57d2ad0b9fd8e09b357701b1e46efe0291979a94
J <juan.villarraza@gmail.com>
2025-02-18 20:47:20 -0300

    fix: audit vulnerabilities (#357)
    * fix: audit vulnerabilities
    
    * update types
    
    * fix workflows
    
    * Fix code style issues with Prettier
    
    ---------
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>

--- src/types/ethers.ts
+++ src/types/ethers.ts
@@ -1,6 +1,3 @@
-import { TransactionResponse as TransactionResponseV5 } from "@ethersproject/abstract-provider";
-import { Signer } from "@ethersproject/abstract-signer";
-import { Wallet } from "@ethersproject/wallet";
 import { ethers } from "ethers";
 
 export type RpcProvider = ethers.JsonRpcProvider;
@@ -8,13 +5,12 @@ export type Contract = ethers.Contract;
 export type ContractRunner = ethers.ContractRunner;
 export type Interface = ethers.Interface;
 
-export type WalletV5 = Signer | Wallet;
 export type WalletV6 = ethers.Signer | ethers.Wallet;
 
-export type EvmWallet = WalletV6 | WalletV5;
+export type EvmWallet = WalletV6;
 
 export type Transaction = ethers.Transaction;
-export type TransactionResponse = ethers.TransactionResponse | TransactionResponseV5;
+export type TransactionResponse = ethers.TransactionResponse;
 export type TransactionRequest = ethers.TransactionRequest;
 
 export type GasData = {
