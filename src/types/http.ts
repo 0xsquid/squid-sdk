dc93f18d7cd2d4e5f2cff8ec9116e1e1d52bc2fd
genaroibc <bonavitaismael@gmail.com>
2025-03-10 14:08:51 -0300

    test: fix tests

--- src/types/http.ts
+++ src/types/http.ts
@@ -4,7 +4,7 @@ import { Config } from ".";
 export type HttpResponse = AxiosResponse;
 
 export interface RequestConfig {
-  baseUrl?: string;
+  baseURL?: string;
   config?: Config;
   headers?: Record<string, string | number | boolean>;
   timeout?: number;
